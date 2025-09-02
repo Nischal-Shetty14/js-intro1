567075d1571f6957a971237df6151549baa54164
Nischal-Shetty14 <173301163+Nischal-Shetty14@users.noreply.github.com>
2025-09-02 17:17:18 +0000

    conversion error

---
+++ basics/conversion.js
@@ -0,0 +1,10 @@
+let num="33"
+console.log(typeof(num))
+let n=Number(num);
+console.log(typeof(n))//number
+console.log(n)//33
+let num2="33abc"
+console.log(typeof(num2))
+let n2=Number(num2)
+console.log(typeof(n2))//number
+console.log(n2)// but here it will give NaN
