730304c92acbaa52ad010fda6c84d9033f370dd2
Nischal-Shetty14 <173301163+Nischal-Shetty14@users.noreply.github.com>
2025-09-18 19:22:28 +0000

    scope

---
+++ basics/scope.js
@@ -0,0 +1,11 @@
+if(true)
+{
+    let a=10;
+    const b=20;
+    var c=30;
+}
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+//a and b don't print coz they are inside the block and not global.C should do the same thing but doesn't so that's why var is an issue 
